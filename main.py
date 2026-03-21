415dd877fa4500983b158e7039661c0c3578c232
Titomi Oni <onititomi@gmail.com>
2026-03-20 19:39:41 -0600

    L3_A1

---
+++ main.py
@@ -0,0 +1,8 @@
+a = 2
+print ("data type of a",type(a)) #output : data type of a <class 'int'>
+b = 2.7
+print ("data type of b", type(b)) 
+c = "Notebook"
+print ("data type of c", type(c))
+d = True
+print ("data type of d", type(d))
