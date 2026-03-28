c023c5338d2c2034f22b08b3cf83ee764bd555e3
Titomi Oni <onititomi@gmail.com>
2026-03-27 20:38:17 -0600

    Congratulations message (Ram)

---
+++ p2.py
@@ -0,0 +1,8 @@
+# Rams orignial message in lower case 👇
+message = "congratulations for getting a gold medal in the state games!" 
+
+# Convert message into uppercase👇
+upper_message = message.upper()
+
+# Finished results👇
+print(upper_message)
